048bb6e4d36606e75ea451087ce28068bb1f256d
Neelakantan K.K <neelakantankk@gmail.com>
2021-12-02 13:55:06 +0530

    Completed Day 02, Part 02

---
+++ Day_02/day_02_p02.py
@@ -0,0 +1,22 @@
+def parse_directions(direction, x_pos, y_pos, aim):
+    if direction[0] == 'forward':
+        return (x_pos + int(direction[1]), y_pos + (aim * int(direction[1])), aim)
+    elif direction[0] == 'down':
+        return (x_pos, y_pos, aim + int(direction[1]))
+    elif direction[0] == 'up':
+        return (x_pos, y_pos, aim - int(direction[1]))
+
+def main():
+    with open('input.txt','r') as infile:
+        directions = [tuple(line.strip().split(" ")) for line in infile.readlines()]
+    
+    x_pos,y_pos, aim = (0,0,0)
+
+    for direction in directions:
+        x_pos, y_pos, aim = parse_directions(direction, x_pos, y_pos, aim)
+    
+    print(x_pos * y_pos)
+    
+
+if __name__ == '__main__':
+    main()
