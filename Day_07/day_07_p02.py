8fb89708a426d35fdda395eec71fef56ba626f19
Neelakantan K.K <neelakantankk@gmail.com>
2021-12-07 21:11:23 +0530

    Modified Day 07, Part 02 to use (n^2 + n)/2 instead of range

--- Day_07/day_07_p02.py
+++ Day_07/day_07_p02.py
@@ -2,13 +2,16 @@ from collections import Counter
 from time import perf_counter_ns
 import statistics
 
+def closed_sum(n):
+    return round(((n**2 +n)/2))
+
 def main():   
     START = perf_counter_ns()
     with open('input.txt','r') as infile:
         alignments = [int(x) for x in infile.read().strip().split(',')]
     
     mean_position = round(statistics.mean(alignments))
-    fuel_consumption_to_align = sum(sum(range(abs(alignment - mean_position)+1)) for alignment in alignments)
+    fuel_consumption_to_align = sum(closed_sum(abs(alignment - mean_position)) for alignment in alignments)
    
     print(fuel_consumption_to_align)
     STOP = perf_counter_ns()
