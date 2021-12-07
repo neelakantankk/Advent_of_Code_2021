7daecf9ee411e5b0382e67c5d4d48281d2f6743c
Neelakantan K.K <neelakantankk@gmail.com>
2021-12-07 14:41:34 +0530

    Modified parts 1 and 2 to utilize median and mean

--- Day_07/day_07_p01.py
+++ Day_07/day_07_p01.py
@@ -1,20 +1,13 @@
 from collections import Counter
-from functools import reduce
+import statistics
 
 def main():   
     with open('input.txt','r') as infile:
         alignments = [int(x) for x in infile.read().strip().split(',')]
     
-    count_of_alignments = Counter(alignments)
-
-    min_fuel_consumption = None
-    
-    for number,count in count_of_alignments.most_common():
-        fuel_consumption_to_align = sum(abs(alignment - number) for alignment in alignments)
-        if min_fuel_consumption is None or min_fuel_consumption > fuel_consumption_to_align:
-            min_fuel_consumption = fuel_consumption_to_align
-
-    print(min_fuel_consumption)
+    median_position = round(statistics.median(alignments))
+    fuel_consumption_to_align = sum(abs(alignment - median_position) for alignment in alignments)   
+    print(fuel_consumption_to_align)
 
 if __name__ == '__main__':
     main()
--- Day_07/day_07_p02.py
+++ Day_07/day_07_p02.py
@@ -1,24 +1,19 @@
 from collections import Counter
 from time import perf_counter_ns
+import statistics
 
 def main():   
     START = perf_counter_ns()
     with open('input.txt','r') as infile:
         alignments = [int(x) for x in infile.read().strip().split(',')]
     
-    possible_positions = range(min(alignments),max(alignments)+1)
-
-    min_fuel_consumption = None
-    
-    for number in possible_positions:
-        fuel_consumption_to_align = sum(sum(range(abs(alignment - number)+1)) for alignment in alignments)        
-        if min_fuel_consumption is None or min_fuel_consumption > fuel_consumption_to_align:
-            min_fuel_consumption = fuel_consumption_to_align
-
-    print(min_fuel_consumption)   
+    mean_position = round(statistics.mean(alignments))
+    fuel_consumption_to_align = sum(sum(range(abs(alignment - mean_position)+1)) for alignment in alignments)
+   
+    print(fuel_consumption_to_align)
     STOP = perf_counter_ns()
 
-    print(f"That took {(STOP - START)/1000} seconds")
+    print(f"That took {(STOP - START)} nanoseconds")
 
 if __name__ == '__main__':
     main()
