e6cda500f65d8a72ed17cc73a814aeb47cf2324e
Neelakantan K.K <neelakantankk@gmail.com>
2021-12-11 22:08:53 +0530

    Modified Day 09, Part 01 to simply the neighbors logic

--- Day_09/day_09_p01.py
+++ Day_09/day_09_p01.py
@@ -3,49 +3,25 @@ from time import perf_counter_ns
 
 Point = namedtuple('Point',['x_pos','y_pos'])
 
-def up_pos(point):
-    return Point(point.x_pos, point.y_pos - 1)
 
-def down_pos(point):
-    return Point(point.x_pos, point.y_pos + 1)
+def get_neighbors(pos,max_rows,max_cols):
+    directions = [(0,1),(1,0),(-1,0),(0,-1)]
+    result = []
 
-def right_pos(point):
-    return Point(point.x_pos +1, point.y_pos)
-
-def left_pos(point):
-    return Point(point.x_pos - 1, point.y_pos)
+    for direction in directions:
+        neighbor = Point(pos.x_pos + direction[0], pos.y_pos + direction[1])
+        if 0 <= neighbor.x_pos < max_cols and 0 <= neighbor.y_pos < max_rows:
+            result.append(neighbor)
+    return result
 
 def is_lower(pos,heights,max_rows,max_cols):
-    x_pos, y_pos = pos
-    if x_pos == 0:
-        if y_pos == 0:
-            adjacents = [right_pos(pos), down_pos(pos)]
-        elif y_pos == (max_rows - 1):
-            adjacents = [right_pos(pos), up_pos(pos)]
-        else:
-            adjacents = [right_pos(pos), up_pos(pos), down_pos(pos)]
-    elif x_pos == (max_cols - 1):
-        if y_pos == 0:
-            adjacents = [left_pos(pos), down_pos(pos)]
-        elif y_pos == (max_rows - 1):
-            adjacents = [left_pos(pos), up_pos(pos)]
-        else:
-            adjacents = [left_pos(pos), up_pos(pos), down_pos(pos)]
-    else:
-        if y_pos == 0:
-            adjacents = [left_pos(pos), down_pos(pos), right_pos(pos)]
-        elif y_pos == (max_rows - 1):
-            adjacents = [left_pos(pos), up_pos(pos), right_pos(pos)]
-        else:
-            adjacents = [left_pos(pos), up_pos(pos), down_pos(pos), right_pos(pos)]
-    
+    adjacents = get_neighbors(pos, max_rows, max_cols)
     adjacent_heights = [heights[pos] for pos in adjacents]
-    
     for height in adjacent_heights:
         if heights[pos] >= height:
             return False
-    
     return True
+
 
 def main():
 
